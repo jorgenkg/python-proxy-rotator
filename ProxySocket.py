72b1329181ca7b3acbab790ec945e1461b3d152c
Jørgen Grimnes <jorgenkg@gmail.com>
2013-01-18 12:34:07 +0100

    Major: readable code
    + Minor:
    -- added sane debug information
    -- some handling improvement

--- ProxySocket.py
+++ ProxySocket.py
@@ -1,8 +1,10 @@
 import threading, collections
-from httplib import HTTPResponse
+import socket
 
 class PROXY_HTTP: pass
 class PROXY_HTTPS: pass
+
+DEBUG = True
 
 class ProxyFetcher():
     http_proxies = collections.deque([
@@ -30,88 +32,66 @@ class ProxyFetcher():
         if count <= 0: return [ ]
         self.lock.acquire()
         
-        random_proxy = self.http_sslproxies[:count] if count > 1 else self.http_sslproxies[0]
-        self.http_sslproxies.rotate(count)
+        random_proxy = [self.http_sslproxies.pop() for i in range(count)]
+        self.http_sslproxies.extendleft(random_proxy)
         
         self.lock.release()
         return random_proxy
 
 proxy_fetcher = ProxyFetcher()
 
-import socket
+
 
 _socket = socket.socket
-
 class ProxySocket(socket.socket):
-    def __init__(self, chainlength=1, family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0, _sock=None):
+    def __init__(self, chainlength=2, family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0, _sock=None):
         _socket.__init__(self, family, type, proto, _sock)
         self._chainlength = chainlength
+    
+    def __chainconnect_server(self, proxy):
+        self.sendall("CONNECT %s:%d HTTP/1.1\r\n\r\n" % proxy)
         
+        resp = self.recv(1)
+        while resp.find("\r\n\r\n")==-1:
+            resp = resp + self.recv(1)
+        statusline = resp.splitlines()[0].split(" ",2)
+        
+        try:
+            if int(statusline[1]) == 200: return True
+        except ValueError, e:
+            print e
+            self.close()
+        if statuscode != 200:
+            self.close()
+            print "ERROR: connection returned %d" % statuscode
+        return False
+     
     def connect(self, server):
-        if self._chainlength > 1:
-            print "* using HTTPS chaining"
-            self.__https_proxies = proxy_fetcher.get_sslproxy(self._chainlength-1)
+        if DEBUG:
+            tmp = int(self._chainlength)-1
+            desired_path = "https proxy --> " * tmp
+            desired_path += "http proxy --> " if self._chainlength>=1 else ""
+            desired_path += "%s:%d" % server
+            print "Path to server: %s" % desired_path
         
-            _socket.connect(self, self.__https_proxies.pop())
-            
-            for proxy in self.__https_proxies:
-                self.sendall("CONNECT %s:%d HTTP/1.1\r\n\r\n" % proxy[1:])
-                
-                resp = self.recv(1)
-                while resp.find("\r\n\r\n")==-1:
-                    resp = resp + self.recv(1)
-                statusline = resp.splitlines()[0].split(" ",2)
-                
-                try:
-                    statuscode = int(statusline[1])
-                except ValueError, e:
-                    print e
-                    self.close()
-                if statuscode != 200:
-                    print "CONNECTION ERROR."
-                    self.close()
-        # end https proxies
-        
-        if self._chainlength > 0:
-            print "* using http proxy --> %s:%d" % server
-            _socket.connect(self, proxy_fetcher.get_proxy()[1:])
-            
-            """
-            Connect to dest
-            """
-            self.sendall("CONNECT %s:%d HTTP/1.1\r\n\r\n" % server)
-                
-            resp = self.recv(1)
-            while resp.find("\r\n\r\n")==-1:
-                resp = resp + self.recv(1)
-            statusline = resp.splitlines()[0].split(" ",2)
-            
-            try:
-                statuscode = int(statusline[1])
-            except ValueError, e:
-                print e
-                self.close()
-            if statuscode != 200:
-                print "CONNECTION ERROR."
-                self.close()
-        # end http proxies
-        else:
-            print "connecting directly to server: %s:%d" % server
+        if self._chainlength == 0:
             _socket.connect(self, server)
-            print "connected."
-            resp = self.recv(1)
-            while resp.find("\r\n\r\n")==-1:
-                resp = resp + self.recv(1)
-            statusline = resp.splitlines()[0].split(" ",2)
-            
-            try:
-                statuscode = int(statusline[1])
-            except ValueError:
-                self.close()
-            if statuscode != 200:
-                print "CONNECTION ERROR."
-                self.close()
-            
+            return
+        
+        if self._chainlength > 1:    
+            # https
+            self.__https_proxies = proxy_fetcher.get_sslproxy( self._chainlength-1 )
+            _socket.connect( self, self.__https_proxies.pop()[1:])
+            # ad hoc
+            for proxy in self.__https_proxies:
+                self.__chainconnect_server(proxy[1:])
+                
+        elif self._chainlength == 1:
+            # http
+            _socket.connect(self, proxy_fetcher.get_proxy()[1:])
+        # dest
+        self.__chainconnect_server(server)
+        
         
         
         
